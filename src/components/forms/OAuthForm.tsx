38312487933f304908630ec9795f224dfd190b90
roger <rch.goldsnaker@gmail.com>
2024-02-15 11:35:44 -0500

    Fixing github outh

--- src/components/forms/OAuthForm.tsx
+++ src/components/forms/OAuthForm.tsx
@@ -8,9 +8,10 @@ import createSupabaseClientClient from "@/lib/supabase/client";
 
 const getURL = () => {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    process?.env?.NEXT_PUBLIC_SITE_URL ? process.env.NEXT_PUBLIC_SITE_URL + '/auth/callback' : // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL + '/auth/callback' : // Automatically set by Vercel.
     'http://localhost:3000/auth/callback'
+  
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`
   // Make sure to include a trailing `/`.
