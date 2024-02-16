84a057a9dce3abdd3e462fb11047b58883cae091
roger <rch.goldsnaker@gmail.com>
2024-02-15 21:35:10 -0500

    Fixing github outh

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -30,7 +30,7 @@ export async function GET(request: Request) {
     )
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      return NextResponse.redirect(`${origin}/test`)
+      return NextResponse.redirect(`${origin}`)
     }
   }
 
